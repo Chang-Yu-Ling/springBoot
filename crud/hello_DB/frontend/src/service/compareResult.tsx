b9964f3de4178d91dbea5aaa30d99d2c7114eb14
Alice <alice310ch@gmail.com>
2023-11-13 02:45:21 +0800

    ControllerAdvice quert=null pass

---
+++ crud/hello_DB/frontend/src/service/compareResult.tsx
@@ -0,0 +1,19 @@
+import axios from 'axios'
+import {pipe} from 'fp-ts/lib/function'
+import * as TE from 'fp-ts/lib/TaskEither'
+
+
+export const getCompareResult=(prodId:string):TE.TaskEither<Error,string>=>
+pipe(
+    TE.tryCatch(
+        ()=>
+        axios.get(`/api/v1/compare?prodId=${prodId}`).then((response)=>{
+            return response.data.CompareResult
+        }),
+        (error)=> {
+            console.log("123",Object.keys(error))
+            //console.log("123",error.ne.reponse.data.message)
+            return new Error(`Failed to get w: ${error}`)
+        }
+    )
+)
